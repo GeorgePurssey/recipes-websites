eab6de087bbab37759ebb00a882fd669d427aeb7
George <76116068+GeorgePurssey@users.noreply.github.com>
2025-01-26 18:46:13 +0000

    Recepies

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+const searchInput = document.querySelector('search-input');
+const searchButton = document.querySelector('search-button');
+const recipesList = document.querySelector('recipes-list');
+
+
+searchButton.addEventListener('click', fetchRecipes);
+searchInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        fetchRecipes();
+    }
+});
+
+
+async function fetchRecipes() {
+        const query = searchInput.value.trim();
+        if (!query) {
+            alert('Please enter a recipe name');
+        }
+
+}
+
 
